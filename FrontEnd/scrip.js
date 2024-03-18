ab38056c2ab31d3ad79230aee8bbe2493269cf25
Jerem.B <Jerem.brunel.33@gmail.com>
2024-03-18 14:52:44 +0100

    ajout des boutons filtres

--- FrontEnd/scrip.js
+++ FrontEnd/scrip.js
@@ -1,10 +1,13 @@
 
-/*Variable*/
+/******Variable*******/
+
 const gallery = document.querySelector(".gallery");
 const body = document.querySelector("body");
-const containerFiltres = document.querySelector(".container-filtres");
+const containerFiltres = document.querySelector(".container-filters");
 
-/* fonction qui retourne le tableau des works*/
+
+/******fonction qui retourne le tableau des works*******/
+
 async function getWorks() {
   const requete = await fetch("http://localhost:5678/api/works");
   return await requete.json();
@@ -16,7 +19,8 @@ async function getCategory() {
   return requete.json();
 }
 async function main() {
-  displayWorksGallery();
+    displayWorksGallery();
+    createAllButtons();
 }
 main();
 
@@ -44,4 +48,35 @@ function createWork(work) {
   figure.appendChild(figcaption);
   gallery.appendChild(figure);
   
+}
+
+
+//**********afiichage des boutons par catégories**************/
+
+//récuperer le tableau des catégories
+
+async function getCategorys() {
+  const response = await fetch("http://localhost:5678/api/categories");
+  return await response.json()
+}
+getCategorys();
+
+//creation des boutons
+
+function createAllButtons() {
+  getCategory().then((data) => {
+    // console.log(data);
+    data.forEach((category) => {
+      createButton(category);
+    });
+  });
+}
+function createButton(category) {
+  const btn = document.createElement("button");
+  btn.classList.add("buttons-filters");
+  btn.textContent = category.name;
+  btn.id = category.id;
+  containerFiltres.appendChild(btn);
+  //console.log(category.id);
+  //console.log(category.name);
 }
