0196d0a53565204bb81261bc63bee73d4fb8d5f2
Jerem.B <Jerem.brunel.33@gmail.com>
2024-03-25 14:50:28 +0100

    ajout connexion et debut modal

--- FrontEnd/js/scripIndex.js
+++ FrontEnd/js/scripIndex.js
@@ -7,9 +7,18 @@ const containerFiltres = document.querySelector(".container-filters");
 const token = window.sessionStorage.getItem("token");
 const user = window.sessionStorage.getItem("userId");
 const logOut = document.getElementById("login-link");
+const sectionPortfolio = document.querySelector("#portfolio");
+const sectionPortfolioH2 = document.querySelector("#portfolio h2");
+const editionText = "Mode édition";
+const editionLogo = `<i class="fa-regular fa-pen-to-square"></i>`;
+const adminConnexionUP = `<div class="admin-edit"><p>${editionLogo} ${editionText}</p></div>`;
+const divEdit = document.createElement("div");
+const spanEdit = document.createElement("span");
+const adminConnexionDown = `${editionLogo}  ${editionText} `;
 
 
 /******fonction qui retourne le tableau des works*******/
+
 async function getWorks() {
   const requete = await fetch("http://localhost:5678/api/works");
   return await requete.json();
@@ -24,11 +33,14 @@ async function main() {
   displayWorksGallery();
   createAllButtons();
   displayByCategory();
+  loginUser();
+  logoutUser();
 }
 main();
 
 
 /*affichage des works dans le dom */
+
 function displayWorksGallery() {
   gallery.innerHTML = "";
   getWorks().then((data) => {
@@ -56,11 +68,18 @@ function createWork(work) {
 //**********afiichage des boutons par catégories**************/
 
 //récuperer le tableau des catégories
+
+async function getWorks() {
+  const requete = await fetch("http://localhost:5678/api/works");
+  return requete.json();
+}
+
 async function getCategorys() {
   const response = await fetch("http://localhost:5678/api/categories");
   return await response.json();
 }
 getCategorys();
+
 
 //creation des boutons
 
@@ -84,6 +103,7 @@ function createButton(category) {
 
 
 // Trie par classe sur les boutons filtres
+
 async function displayByCategory() {
   const works = await getWorks();
   const buttons = document.querySelectorAll(".container-filters"); //probleme selecteur (pas tous les boutons)
@@ -95,7 +115,7 @@ async function displayByCategory() {
       });
       button.classList.add("active");
       const btnId = e.target.id;
- 
+
       gallery.innerHTML = "";
       works.forEach((work) => {
         if (btnId == work.categoryId) {
@@ -106,5 +126,37 @@ async function displayByCategory() {
         }
       });
     });
+  });
+}
+
+
+/*****Page utilisateur conecté*****/
+
+function loginUser() {
+  if (user) {
+    logOut.textContent = "logout";
+    document.body.insertAdjacentHTML("afterbegin", adminConnexionUP);
+    spanEdit.innerHTML = adminConnexionDown;
+    divEdit.classList.add("div-edit");
+    divEdit.appendChild(sectionPortfolioH2);
+    divEdit.appendChild(spanEdit);
+    sectionPortfolio.prepend(divEdit);
+    containerFiltres.style = "display:none";
+  } else {
+    //console.log("L'utilisateur n'est pas connecté");
+  }
+}
+
+
+/*****Page utilisateur déconecté*****/
+
+function logoutUser() {
+  logOut.addEventListener("click", () => {
+    if (user) {
+      window.sessionStorage.setItem("token", "");
+      logOut.textContent = "login";
+      window.sessionStorage.setItem("userId", "");
+      window.location.href = "index.html";
+    }
   });
 }
--- FrontEnd/js/scriptLogin.js
+++ FrontEnd/js/scriptLogin.js
@@ -8,42 +8,45 @@ const logOut = document.getElementById("login-link");
 
 /***recupération email et password***/
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const userEmail = email.value;
-    const userPassword = password.value;
-    const login = {
-        email: userEmail,
-        password: userPassword
-    };
-    const user = JSON.stringify(login);
+  e.preventDefault();
+  const userEmail = email.value;
+  const userPassword = password.value;
+  const login = {
+    email: userEmail,
+    password: userPassword,
+  };
+  const user = JSON.stringify(login);
 
-    /**envoi requette**/
-    fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        mode: "cors",
-        credentials: "same-origin",
-        headers: {"Content-Type": "application/json" },
-        body: user,
-})
+  /**envoi requette**/
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    mode: "cors",
+    credentials: "same-origin",
+    headers: { "Content-Type": "application/json" },
+    body: user,
+  })
     /**recupération réponse**/
     .then((response) => {
-        if (!response.ok) {
-            email.style.border = "2px solid #FF0000";
-            password.style.border = "2px solid #FF0000";
-            const wrongLogin = document.querySelector(".p-wrong");
-            wrongLogin.textContent = "Le mot de passe ou l'identifiant que vous avez fourni est incorrect.";
-              throw new Error("Le mot de passe ou l'identifiant que vous avez fourni est incorrect.");
-        }
-        return response.json();
+      if (!response.ok) {
+        email.style.border = "2px solid #FF0000";
+        password.style.border = "2px solid #FF0000";
+        const wrongLogin = document.querySelector(".p-wrong");
+        wrongLogin.textContent =
+          "Le mot de passe ou l'identifiant que vous avez fourni est incorrect.";
+        throw new Error(
+          "Le mot de passe ou l'identifiant que vous avez fourni est incorrect."
+        );
+      }
+      return response.json();
     })
     .then((data) => {
-        const userId = data.userId;
-        const userToken = data.token;
-        window.sessionStorage.setItem("token", userToken);
-        window.sessionStorage.setItem("userId", userId);
-        window.location.href = "index.html";
+      const userId = data.userId;
+      const userToken = data.token;
+      window.sessionStorage.setItem("token", userToken);
+      window.sessionStorage.setItem("userId", userId);
+      window.location.href = "index.html";
     })
     .catch((error) => {
-        console.error("Une erreur est survenue : ",error);
-    })
+      console.error("Une erreur est survenue : ", error);
     });
+});
