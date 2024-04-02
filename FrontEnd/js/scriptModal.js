23f262c2e3acb01c41de3e9c54a28eb092299593
Jerem.B <Jerem.brunel.33@gmail.com>
2024-04-02 14:52:43 +0200

    construction et ajout modale 2

--- FrontEnd/js/scriptModal.js
+++ FrontEnd/js/scriptModal.js
@@ -1,24 +1,50 @@
 //fonction principale
 const containerModal = document.querySelector("#container-modal");
 const modalGallery = document.querySelector(".modal-gallery");
-const xmark = document.querySelector("#container-modal .fa-xmark");
+
+//Variables pour l'affichage de la deuxieme mmodale partie
 const buttonAddPhoto = document.querySelector(".modal-button button");
 const modalPortfolio = document.querySelector(".modal");
 const modalAddWorks = document.querySelector(".modalAddWorks");
+//Variables Pour le form
+const formAddWorks = document.querySelector("#formAddWorks");
+const labelFile = document.querySelector("#formAddWorks label");
+const paragraphFile = document.querySelector("#formAddWorks p");
+const inputTitle = document.querySelector("#title");
+const inputCategory = document.querySelector("#categoryInput");
+const inputFile = document.querySelector("#file");
+const previewImage = document.getElementById("previewImage");
 
-//affichage de la modale au click sur le bouton modifier
+//Fonction Principale pour l'affichage des works dans la Modale
+function mainModal() {
+  if (user) {
+    displayWorksGallery();
+    displayModalAddWorks();
+    returnToModalPortfolio();
+  }
+}
+
+//affichage de la modale au click sur le bouton modifier / je n'arrive pas creer une fonction pour cette partie
+
 spanEdit.addEventListener("click", function () {
-  console.log("spanEdith");
+  //console.log("spanEdith");
   containerModal.style.display = "flex";
   modalPortfolio.style.display = "flex";
   modalAddWorks.style.display = "none";
-
 });
 
-//Fermuture de la modal sur la croix
+//Fermuture de la modal sur la croix 1 / je n'arrive pas creer une fonction pour cette partie
+
+const xmark = document.querySelector("#container-modal .fa-xmark");
 xmark.addEventListener("click", function () {
-  console.log("xmark");
   containerModal.style.display = "none";
+});
+
+//Fermuture de la modal sur la croix 2
+const xmark2 = document.querySelector(".modalAddWorks .fa-xmark");
+xmark2.addEventListener("click", function () {
+  containerModal.style.display = "none";
+  modalAddWorks.style.display = "none";
 });
 
 //Fermeture de la modal sur le container grisé
@@ -29,25 +55,78 @@ containerModal.addEventListener("click", function (e) {
 });
 sectionPortfolio.appendChild(containerModal);
 
-//récupération des works & appel de la fonction de création de works dans la gallery
-async function displayGallery () {
-  modalGallery.innerHTML = ""
-  const gallery = await getWorks()
-  console.log(gallery);
-  gallery.forEach(works => {
-    const figure = document.createElement("figure")
-    const img = document.createElement("img")
-    const span = document.createElement("span")
-    const trash = document.createElement("i")
-    trash.classList.add("fa-solid", "fa-trash-can")
-    trash.id = works.id
-    img.src = works.imageUrl
-    img.alt = works.title
-    span.appendChild(trash)
-    figure.appendChild(img)
-    figure.appendChild(span)
-    modalGallery.appendChild(figure)
+//affichage de la gallerie dans la modale
+async function displayGallery() {
+  modalGallery.innerHTML = "";
+  const gallery = await getWorks();
+  gallery.forEach((works) => {
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const span = document.createElement("span");
+    const trash = document.createElement("i");
+    trash.classList.add("fa-solid", "fa-trash-can");
+    trash.id = works.id;
+    img.src = works.imageUrl;
+    img.alt = works.title;
+    span.appendChild(trash);
+    figure.appendChild(img);
+    figure.appendChild(span);
+    modalGallery.appendChild(figure);
+  });
+  deletePhoto(); //appel de la fonction deletePhoto apres la creation de la gallerie et des poubelles
+}
+displayGallery();
+
+//Objet de paramétrage pour requette DELETE avec token
+const deleteWorkID = {
+  method: "DELETE",
+  headers: {
+    Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json",
+  },
+  mode: "cors",
+  credentials: "same-origin",
+};
+
+//suppression d'une photo dans la modale
+function deletePhoto() {
+  const trashs = document.querySelectorAll(".fa-trash-can");
+  // console.log(trashs);
+  trashs.forEach((trash) => {
+    trash.addEventListener("click", (e) => {
+      const workID = trash.id;
+      // console.log(trash);
+      fetch(`http://localhost:5678/api/works/${workID}`, deleteWorkID).then(
+        () => {
+          displayGallery();
+          displayWorksGallery();
+        }
+      );
+    });
   });
 }
-displayGallery()
 
+//fonction d'affichage au click sur btn:"ajouter-photo" de la modalAddWorks
+function displayModalAddWorks() {
+  buttonAddPhoto.addEventListener("click", () => {
+    containerModal.style.display = "flex";
+    modalPortfolio.style.display = "none";
+    modalAddWorks.style.display = "flex";
+  });
+}
+displayModalAddWorks();
+
+// Retour sur modal depuis la flèche de la modalAddWorks
+function returnToModalPortfolio() {
+  const arrowLeftModalWorks = document.querySelector(
+    ".modalAddWorks .fa-arrow-left"
+  );
+  arrowLeftModalWorks.addEventListener("click", () => {
+    //Supréssion de la prewiew a clik sur retour dans la modale
+    inputFile.value = "";
+    containerModal.style.display = "flex";
+    modalPortfolio.style.display = "flex";
+    modalAddWorks.style.display = "none";
+  });
+}
+returnToModalPortfolio();
